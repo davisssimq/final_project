2680d01000a2de1101e47d1811f01c7092bcdd7f
VadymMashliuk <maslukvadim810@gmail.com>
2026-05-04 13:09:38 +0200

    Update SablonaWeboveStranky.php

--- app/Views/SablonaWeboveStranky.php
+++ app/Views/SablonaWeboveStranky.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?= base_url('node_modules/bootstrap/dist/css/bootstrap.min.css') ?>">
 </head>
 <body>
     <?= $this->renderSection('sekce') ?>
+    <script src="<?= base_url('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
 </body>
 </html>
