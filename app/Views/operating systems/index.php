4a5d2cce5bb7f6cb3373e23928d70c223f356ecb
VadymMashliuk <maslukvadim810@gmail.com>
2026-05-25 12:01:27 +0200

    Přidání možnosti odkazování na hry, které patří do konkrétního OS

--- app/Views/operating systems/index.php
+++ app/Views/operating systems/index.php
@@ -5,7 +5,7 @@
     <div class="col-md-4">
         <div class="card bg-dark text-light border-secondary h-100">
             <div class="card-body">
-                <h5 class="fw-bold mb-2"><?= $operacniSystem['name'] ?></h5>
+                <a href="<?= site_url('operating_systems/games/'.$operacniSystem['id_operating_system']) ?>"><h5 class="fw-bold mb-2"><?= $operacniSystem['name'] ?></h5></a>
                 <p class="small text-secondary mb-2"><?= $operacniSystem['description'] ?></p>
             </div>
         </div>
