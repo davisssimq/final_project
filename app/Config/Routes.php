e22aad80cc971a4cee3f1dfdce4dba6b19d888e1
VadymMashliuk <132168251+VadymMashliuk@users.noreply.github.com>
2026-05-25 11:21:47 +0200

    Přidání rout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,3 +12,6 @@ $routes->post('/games/store', 'GameController::store');
 $routes->get('/games/edit/(:num)', 'GameController::edit/$1');
 $routes->post('/games/update/(:num)', 'GameController::update/$1');
 $routes->post('/games/delete/(:num)', 'GameController::delete/$1');
+
+$routes->get('operating_systems', 'Operating_systemController::index');
+$routes->get('/operating_systems/games/(:num)', 'Operating_systemController::games/$1');
